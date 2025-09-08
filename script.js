fae6a37adbcc78b1e15c6caa74998b46b4023880
Akrame-Amine Dridi <aadridi@gmail.com>
2025-09-08 18:37:27 +0200

    Create basic HTML + CSS + JS structure
    Starting to implement basic functions/operations

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1. Basic math operations
+
+// Addition
+const add = function (a, b) {
+	return a + b;
+};
+
+// Soustraction
+const substract = function (a, b) {
+	return a - b;
+};
+
+// Multiplication
+const multiply = function (a, b) {
+	return a * b;
+};
+
+// Division
+const divide = function (a, b) {
+	return a / b;
+};
+
+// 2. Function operate(operator, number1, number2)
